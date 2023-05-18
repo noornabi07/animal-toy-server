9fb64290417cc972d22f074487633e0acca79fe1
noornabi07 <noornabiprogram07@gmail.com>
2023-05-19 02:11:00 +0600

    get the api with specific email and using clients site

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/myToys/:email', async(req, res) =>{
+        console.log(req.params.email)
+        const result = await toyCollection.find({email: req.params.email}).toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
